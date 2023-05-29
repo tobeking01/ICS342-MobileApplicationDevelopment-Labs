72ed7860f2abbfdcffc1706a70fa662a84f7fa39
tobeking01 <78264849+tobeking01@users.noreply.github.com>
2023-05-29 18:15:41 -0500

    Lab2 Changes 2 pushed 5/29/23

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.ics342.labs
-
-import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.DialogFragment
 
 class MainActivity : AppCompatActivity() {
 
